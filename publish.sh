9b322eacfb2a491a27e4aa525a8d77f50375165d
Mark Otway <mark@otway.com>
2025-01-08 10:31:06 +0000

    Use the UK version of Humaniser

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-dotnet publish SolisManager.sln -r osx-x64 -c Release
+dotnet publish SolisManager.sln --self-contained true -r osx-x64 -c Release
 
-dotnet publish SolisManager.sln -r linux-x64 -c Release
+dotnet publish SolisManager.sln --self-contained true -r linux-x64 -c Release
 
-dotnet publish SolisManager.sln -r win-x64 -c Release
+dotnet publish SolisManager.sln --self-contained true -r win-x64 -c Release
